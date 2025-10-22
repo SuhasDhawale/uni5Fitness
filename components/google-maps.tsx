ca31814d0ea256790a2836ec27bee22fd4a340e0
SuhasDhawale <suhasdhawale57@gmail.com>
2025-10-22 22:52:53 +0530

    added map links

--- components/google-maps.tsx
+++ components/google-maps.tsx
@@ -3,35 +3,44 @@
 import { useState } from "react"
 
 export default function GoogleMaps() {
-  const [selectedBranch, setSelectedBranch] = useState("brahmapuri")
-
   const branches = {
     brahmapuri: {
       name: "Brahmapuri Branch",
-      address: "Shantanu Building Ground floor, Tilak Nagar, Wadsa Road Brahmapuri MH- 441206",
+      address:
+        "Shantanu Building, Ground Floor, Tilak Nagar, Wadsa Road, Brahmapuri, MH - 441206",
       embedUrl:
-        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3721.5!2d79.9!3d20.6!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjDCsDM2JzAwLjAiTiA3OcKwNTQnMDAuMCJF!5e0!3m2!1sen!2sin!4v1234567890",
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3721.515!2d79.878!3d20.612!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a2d0d6b7c4c3fd7%3A0xb7b8f9e47e1a5e1!2sBrahmapuri%2C%20Maharashtra%20441206!5e0!3m2!1sen!2sin!4v1730000000000!5m2!1sen!2sin",
     },
     armori: {
       name: "Armori Branch",
-      address: "Behind Chandu Kirana Store, 1st floor Above Bank of India, Armori MH - 441208",
+      address:
+        "Behind Chandu Kirana Store, 1st Floor, Above Bank of India, Armori, MH - 441208",
       embedUrl:
-        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3721.5!2d79.8!3d20.7!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjDCsDQyJzAwLjAiTiA3OcKwNDgnMDAuMCJF!5e0!3m2!1sen!2sin!4v1234567890",
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3720.933!2d79.870!3d20.694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a2d13e7461c86c3%3A0xe7f80d8c76eb0d4b!2sArmori%2C%20Maharashtra%20441208!5e0!3m2!1sen!2sin!4v1730000000001!5m2!1sen!2sin",
     },
   }
 
+  type BranchKey = keyof typeof branches
+  const [selectedBranch, setSelectedBranch] = useState<BranchKey>("brahmapuri")
+
+  const currentBranch = branches[selectedBranch]
+
   return (
     <div className="bg-gray-900 p-8 rounded-lg border border-gray-800">
-      <h3 className="text-2xl font-bold text-white mb-6 text-center">Find Our Locations</h3>
+      <h3 className="text-2xl font-bold text-white mb-6 text-center">
+        Find Our Locations
+      </h3>
 
       {/* Branch Selector */}
-      <div className="flex justify-center gap-4 mb-6">
+      <div className="flex justify-center flex-wrap gap-4 mb-6">
         {Object.entries(branches).map(([key, branch]) => (
           <button
             key={key}
-            onClick={() => setSelectedBranch(key)}
+            onClick={() => setSelectedBranch(key as BranchKey)}
             className={`px-6 py-3 rounded-lg font-semibold transition-all ${
-              selectedBranch === key ? "bg-yellow-500 text-black" : "bg-gray-800 text-white hover:bg-gray-700"
+              selectedBranch === key
+                ? "bg-yellow-500 text-black"
+                : "bg-gray-800 text-white hover:bg-gray-700"
             }`}
           >
             {branch.name}
@@ -42,7 +51,7 @@ export default function GoogleMaps() {
       {/* Map */}
       <div className="relative h-96 rounded-lg overflow-hidden">
         <iframe
-          src={branches[selectedBranch as keyof typeof branches].embedUrl}
+          src={currentBranch.embedUrl}
           width="100%"
           height="100%"
           style={{ border: 0 }}
@@ -53,9 +62,12 @@ export default function GoogleMaps() {
         />
       </div>
 
+      {/* Address */}
       <div className="mt-4 text-center">
-        <h4 className="text-lg font-bold text-white mb-2">{branches[selectedBranch as keyof typeof branches].name}</h4>
-        <p className="text-gray-400">{branches[selectedBranch as keyof typeof branches].address}</p>
+        <h4 className="text-lg font-bold text-white mb-2">
+          {currentBranch.name}
+        </h4>
+        <p className="text-gray-400">{currentBranch.address}</p>
       </div>
     </div>
   )
